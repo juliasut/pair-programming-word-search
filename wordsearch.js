e68226940e7ba4db4ac3933cdb8d0b10a504e13e
juliasut <juliasutygina@gmail.com>
2021-06-30 23:11:45 -0600

    Add pair programming solution for Wordsearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(letters) {
+    let arr = [];
+    for (let x = 0; x < letters.length; x++) {
+      for (let y = 0; y < letters[x].length; y++) {
+        if (!arr[y]) arr[y] = [];
+        let element = letters[x][y];
+        arr[y][x] = element;
+      }
     }
-}
-
+    return arr;
+  } 
+  
+  const wordSearch = (letters, word) => {
+      const horizontalJoin = letters.map(ls => ls.join(''));
+      const transposed = transpose(letters);
+      const verticalJoin = transposed.map(ls => ls.join(''));
+      const rHorizontalJoin = letters.map(ls => ls.reverse().join(''));
+      const rVerticalJoin = transposed.map(ls => ls.reverse().join(''));
+      
+      for (let l of horizontalJoin) {
+  
+          if (l.includes(word)) {
+              return true;
+          }
+      }
+      for (let r of verticalJoin) {
+          if(r.includes(word)) {
+              return true;
+          }
+      }
+      for (let l of rHorizontalJoin) {
+  
+          if (l.includes(word)) {
+              return true;
+          }
+      }
+      for (let r of rVerticalJoin) {
+          if(r.includes(word)) {
+              return true;
+          }
+      }
+      return false;
+  };
+  
+  
 module.exports = wordSearch
+  
+  
